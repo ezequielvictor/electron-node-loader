bee5bf61a33406f3ec881ad4ed0cd50370205fc3
Victor Ezequiel <victor.ezequiel0@gmail.com>
2023-01-14 16:40:45 -0400

    loader to use electron-overlay

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const loaderUtils = require('loader-utils');
 
-module.exports = function() {
-	var query = loaderUtils.getOptions(this);
+module.exports = function () {
+  var query = loaderUtils.getOptions(this);
 
-	var outfile = loaderUtils.interpolateName(this, query.folder + '/[name].[ext]', {
-		context: this.context
-	});
-	var path = '../'
+  var outfile = loaderUtils.interpolateName(this, query.folder + '/[name].[ext]', {
+    context: this.context,
+  });
+  var path = '../';
 
-	if (query && query.prod) {
-		outfile = 'resources/' + outfile;
-		path += '../../';
-	}
+  if (query && query.prod) {
+    return `try {global.process.dlopen(module, process.resourcesPath + '/${outfile}'); } catch(e) {
+			throw new Error('Cannot open ${outfile}: ' + e);}`;
+  }
 
-	return `try {global.process.dlopen(module, require('path').dirname(__filename) + '/${path}${outfile}'); } catch(e) {
+  return `try {global.process.dlopen(module, require('path').dirname(__filename) + '/${path}${outfile}'); } catch(e) {
 	throw new Error('Cannot open ${outfile}: ' + e);}`;
 };
-
